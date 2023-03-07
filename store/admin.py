c9764d1157ce22774ca8b57a9ecb21c706c8b682
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-07 17:20:27 +0100

    Configure admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "slug", "price", "in_stock", "created", "updated"]
+    list_filter = ["in_stock", "is_active"]
+    list_editable = ["price", "in_stock"]
+    prepopulated_fields = {"slug": ("title",)}
