1f7fab8c0e8b8d3c49a033269d69b72d4ce5fff7
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-08 18:30:35 +0100

    Update models.py

--- store/models.py
+++ store/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to="images/")
     slug = models.SlugField(max_length=255)
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
