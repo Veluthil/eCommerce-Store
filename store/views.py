c9009dca5dc5c326ac0d9954d5b53f93bf7af1fd
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-08 18:31:05 +0100

    Add functions

--- store/views.py
+++ store/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "store/home.html", {"products": products})
+
+
+def all_categories(request):
+    return {"categories": Category.objects.all()}
