b89ec97affbb9f634bdad0647299606910295c55
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-10 00:20:31 +0100

    Add allowed hosts and set static files directories

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-k3-pwwb64stm&xudp+#sgcx38wt2&iy+97gu!o&(h(=mox(cto
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourdomain.com', '127.0.0.1']
 
 
 # Application definition
@@ -52,7 +52,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'store.views.all_categories',
+                'store.context_processors.all_categories',
             ],
         },
     },
@@ -106,7 +106,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
