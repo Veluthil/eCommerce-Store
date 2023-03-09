a2a41a30502c97c4d1b0ec918ca75711508b028a
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-10 00:21:09 +0100

    Update names of functions

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    path("", views.all_products, name="all_products"),
-    path("product/<slug:slug>/", views.product_detail, name="product_detail"),
-    path("search/<slug:category_slug>/", views.category_list, name="category_list")
+    path("", views.product_all, name="product_all"),
+    path("<slug:slug>", views.product_detail, name="product_detail"),
+    path("shop/<slug:category_slug>/", views.category_list, name="category_list")
 ]
--- store/views.py
+++ store/views.py
@@ -3,18 +3,14 @@ from django.shortcuts import get_object_or_404, render
 from .models import Category, Product
 
 
-def all_categories(request):
-    return {"categories": Category.objects.all()}
-
-
-def all_products(request):
-    products = Product.objects.all()
+def product_all(request):
+    products = Product.products.all()
     return render(request, "store/home.html", {"products": products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, "store/products/detail.html", {"product": product})
+    return render(request, "store/products/single_product.html", {"product": product})
 
 
 def category_list(request, category_slug):
