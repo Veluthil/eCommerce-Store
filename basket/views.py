198aeb1aadf251962019d1d4cd629b2580182eb4
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-13 22:35:18 +0100

    Add basket_add function

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+
+from basket.basket import Basket
+from store.models import Product
 
 
 def basket_summary(request):
     return render(request, "store/basket/summary.html")
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
+        response = JsonResponse({"test": "data"})
+        return response
