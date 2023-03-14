f0e0546087615990b1a4cc45596e12921cdee75e
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-14 22:39:39 +0100

    Add __iter__ and get_subtotal methods to the Basket class

--- basket/basket.py
+++ basket/basket.py
@@ -1,7 +1,10 @@
+from store.models import Product
+
+from decimal import Decimal
 
 
 class Basket:
-    """A base Basket class that provides some default behaviors that can be inherited or overrided, as necessary."""
+    """A base Basket class that provides some default behaviors that can be inherited or overridden, as necessary."""
 
     def __init__(self, request):
         self.session = request.session
@@ -17,6 +20,22 @@ class Basket:
             self.basket[product_id] = {"price": str(product.price), "qty": int(product_qty)}
         self.session.modified = True
 
+    def __iter__(self):
+        """Collect the product_id in the session data to query the database and return products."""
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+        for product in products:
+            basket[str(product.id)]["product"] = product
+        for item in basket.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
+            yield item
+
     def __len__(self):
         """Get the basket data and count the quantity of items."""
         return sum(item["qty"] for item in self.basket.values())
+
+    def get_subtotal(self):
+        """Get the total price of items in the basket."""
+        return sum(Decimal(item["price"]) * item["qty"] for item in self.basket.values())
