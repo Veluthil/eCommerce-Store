b25d2362839cfedfbda6558d8e5a6875c2971f15
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-15 19:39:40 +0100

    Add delete/ path to the basket urlpatterns

--- basket/urls.py
+++ basket/urls.py
@@ -7,4 +7,5 @@ app_name = "basket"
 urlpatterns = [
     path("", views.basket_summary, name="basket_summary"),
     path("add/", views.basket_add, name="basket_add"),
+    path("delete/", views.basket_delete, name="basket_delete"),
 ]
