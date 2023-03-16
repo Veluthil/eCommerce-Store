902fdc5c09dec5503060dc96e8c269f8e8fadad2
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-16 01:28:44 +0100

    Add update/ path in basket urlpatterns

--- basket/urls.py
+++ basket/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.basket_summary, name="basket_summary"),
     path("add/", views.basket_add, name="basket_add"),
     path("delete/", views.basket_delete, name="basket_delete"),
+    path("update/", views.basket_update, name="basket_update"),
 ]
