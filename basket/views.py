4a66d2ef3f556eec8efe1366aedd97f4a64ea378
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-16 01:30:44 +0100

    Add basket_update function

--- basket/views.py
+++ basket/views.py
@@ -24,13 +24,24 @@ def basket_add(request):
 
 def basket_delete(request):
     basket = Basket(request)
-    if request.POST.get('action') == 'post':
-        product_id = int(request.POST.get('productid'))
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
         basket.delete(product=product_id)
 
-        basketqty = basket.__len__()
-        baskettotal = basket.get_subtotal()
-        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        basket_qty = basket.__len__()
+        basket_total = basket.get_subtotal()
+        response = JsonResponse({"qty": basket_qty, "subtotal": basket_total})
         return response
 
 
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+        basket.update(product=product_id, qty=product_qty)
+
+        basket_qty = basket.__len__()
+        basket_total = basket.get_subtotal()
+        response = JsonResponse({"qty": basket_qty, "subtotal": basket_total})
+        return response
