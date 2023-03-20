612be74d3d0f09976a3b319798366118f3f9ea1e
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-20 20:45:11 +0100

    Create account application

---
+++ account/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import UserBase
+
+admin.site.register(UserBase)
---
+++ account/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django_countries.fields import CountryField
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomAccountManager(BaseUserManager):
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+        other_fields.setdefault("is_staff", True)
+        other_fields.setdefault("is_superuser", True)
+        other_fields.setdefault("is_active", True)
+        if other_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must be assigned to is_staff=True")
+        if other_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must be assigned to is_superuser=True")
+        return self.create_user(email, user_name, password, **other_fields)
+
+    def create_user(self, email, user_name, password, **other_fields):
+        if not email:
+            raise ValueError(_("You must provide an email address to continue."))
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_("email address"), unique=True)
+    user_name = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    surname = models.CharField(max_length=100, blank=True)
+    about = models.TextField(_("about"), max_length=500, blank=True)
+
+    # Delivery details
+    country = CountryField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    postcode = models.CharField(max_length=10, blank=True)
+    address_1 = models.CharField(max_length=100, blank=True)
+    address_2 = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+
+    # User status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["user_name"]
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+    def __str__(self):
+        return self.user_name
