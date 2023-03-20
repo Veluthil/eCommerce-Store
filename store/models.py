3722c0698d83fce3615652932ca394e7ee29a3f8
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-20 20:48:01 +0100

    Import settings from django.conf

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+from django.conf import settings
 
 
 class ProductManager(models.Manager):
@@ -27,7 +27,7 @@ class Category(models.Model):
 class Product(models.Model):
 
     category = models.ForeignKey(Category, related_name="product", on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, related_name="product_creator", on_delete=models.CASCADE)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="product_creator", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default="admin")
     description = models.TextField(blank=True)
