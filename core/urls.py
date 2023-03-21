cce6bfb8fe70bf312f381cf515d06c89ee582560
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-21 20:31:43 +0100

    Add path to account/ in the urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls", namespace="store")),
-    path("basket/", include("basket.urls", namespace="basket"))
+    path("basket/", include("basket.urls", namespace="basket")),
+    path("account/", include("account.urls", namespace="account")),
 ]
 
 if settings.DEBUG:
