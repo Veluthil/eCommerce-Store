cc9f6a5725fd59d8c592d149f5216f4bfa51938a
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-22 20:02:52 +0100

    Add paths for activation and dashboard in the urlpatterns

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,6 @@ app_name = "account"
 
 urlpatterns = [
     path("register/", views.account_register, name="register"),
+    path("activate/<slug:uidb64>/<slug:token>", views.account_activate, name="activate"),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
