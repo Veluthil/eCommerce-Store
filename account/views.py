2a9a03643094df9497cf6d21e0e90a11b71e57f1
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-22 20:03:52 +0100

    Add dashboard and account_activate functions. Add new imports

--- account/views.py
+++ account/views.py
@@ -1,16 +1,25 @@
+from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.http import HttpResponse
 
 from .forms import RegistrationForm
+from .models import UserBase
 from .token import account_activation_token
+
+
+@login_required()
+def dashboard(request):
+    return render(request, "account/user/dashboard.html")
 
 
 def account_register(request):
     # if request.user.is_authenticated:
-    #     return redirect("/")
+    #     return redirect('account:dashboard')
     if request.method == "POST":
         register_form = RegistrationForm(request.POST)
         if register_form.is_valid():
@@ -19,7 +28,6 @@ def account_register(request):
             user.set_password(register_form.cleaned_data["password"])
             user.is_active = False
             user.save()
-            # Email setup
             current_site = get_current_site(request)
             subject = "Activate your Account"
             message = render_to_string("account/registration/account_activation_email.html", {
@@ -29,6 +37,23 @@ def account_register(request):
                 "token": account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
+            print(message)
+            return HttpResponse("registered successfully and activation sent")
     else:
         register_form = RegistrationForm()
     return render(request, "account/registration/register.html", {"form": register_form})
+
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+        if user is not None and account_activation_token.check_token(user, token):
+            user.is_active = True
+            user.save()
+            login(request, user)
+            return redirect("account:dashboard")
+        else:
+            return render(request, "account/registration/invalid_activation.html")
+    except(TypeError, ValueError, OverflowError):
+        user = None
