369509ed0e7e67fb593c7b544755cde3bb58a122
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-24 17:05:10 +0100

    Add PwdResetForm and PwdResetConfirmForm classes

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 from django_countries.fields import CountryField
 
 from .models import UserBase
@@ -159,3 +159,42 @@ class UserEditForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["user_name"].required = True
         self.fields["email"].required = True
+
+
+class PwdResetForm(PasswordResetForm):
+    email = forms.EmailField(max_length=254, widget=forms.TextInput(
+        attrs={
+            "class": "form-control mb-3",
+            "placeholder": "Email",
+            "id": "form-email",
+        }
+    ))
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        u = UserBase.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError(
+                "We could not find this account, sorry."
+            )
+        return email
+
+
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label="New password", widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "New Password",
+                "id": "form-newpassword1",
+            }
+        ))
+
+    new_password2 = forms.CharField(
+        label="Repeat password", widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control mb-3",
+                "placeholder": "New Password",
+                "id": "form-newpassword2",
+            }
+        ))
