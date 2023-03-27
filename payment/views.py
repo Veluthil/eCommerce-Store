9eae464b5691e95ec729b857061cbf80be8d3419
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-27 22:50:18 +0200

    Create basket_view function

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from basket.basket import Basket
+
+import stripe
+
 
 @login_required()
 def basket_view(request):
-    return render(request, "payment/payment_start.html")
+    basket = Basket(request)
+    total = str(basket.get_total_price())
+    total = total.replace(".", "")
+    total = int(total)
+
+    stripe.api_key = "INSERT YOUR STRIPE SECRET KEY HERE"
+    intent = stripe.PaymentIntent.create(
+        amount=total,
+        currency="usd",
+        metadata={"userid": request.user.id}
+    )
+    return render(request, "payment/payment_start.html", {"client_secret": intent.client_secret})
+
+
