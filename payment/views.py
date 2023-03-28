f09bb767ffe59394e54ebebfb53e6bb5195bf375
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-28 22:54:47 +0200

    Add stripe_webhook and order_placed functions

--- payment/views.py
+++ payment/views.py
@@ -1,7 +1,13 @@
+import json
+
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 from basket.basket import Basket
+from core.settings import SECRET_KEY_STRIPE
+from orders.views import payment_confirmation
 
 import stripe
 
@@ -13,7 +19,7 @@ def basket_view(request):
     total = total.replace(".", "")
     total = int(total)
 
-    stripe.api_key = "INSERT YOUR STRIPE SECRET KEY HERE"
+    stripe.api_key = SECRET_KEY_STRIPE
     intent = stripe.PaymentIntent.create(
         amount=total,
         currency="usd",
@@ -22,3 +28,34 @@ def basket_view(request):
     return render(request, "payment/payment_start.html", {"client_secret": intent.client_secret})
 
 
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            json.loads(payload), stripe.api_key
+        )
+    except ValueError as e:
+        print(e)
+        return HttpResponse(status=400)
+
+    # Handle the event
+    if event.type == "payment_intent.succeeded":
+        payment_confirmation(event.data.object.client_secret)
+    else:
+        print(f"Unhandled event type {event.type}")
+
+    return HttpResponse(status=200)
+
+
+def order_placed(request):
+    basket = Basket(request)
+    subject = "Order has been placed successfully."
+    message = "We confirm that your order has been placed.\n " \
+              "Thank you for shopping with us!\n" \
+              "Dokushou Vernissage Team"
+    request.user.email_user(subject=subject, message=message)
+    basket.clear()
+    return render(request, "payment/orderplaced.html")
