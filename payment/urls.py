59a0bd11b41ac9bcf636567213bec0aec2ed9122
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-28 22:56:08 +0200

    Add paths for orderplaced/ and webhook/

--- payment/urls.py
+++ payment/urls.py
@@ -6,4 +6,6 @@ app_name = "payment"
 
 urlpatterns = [
     path("", views.basket_view, name="basket"),
+    path("orderplaced/", views.order_placed, name="order_placed"),
+    path("webhook/", views.stripe_webhook),
 ]
