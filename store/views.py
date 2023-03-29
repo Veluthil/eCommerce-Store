15491723b04ed01871ddcd38c230ef840b8ace41
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-29 21:32:13 +0200

    Update views.py of store

--- store/views.py
+++ store/views.py
@@ -5,15 +5,15 @@ from .models import Category, Product
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, "store/home.html", {"products": products})
+    return render(request, "store/index.html", {"products": products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, "store/products/single_product.html", {"product": product})
+    return render(request, "store/single_product.html", {"product": product})
 
 
 def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
-    return render(request, "store/products/category.html", {"category": category, "products": products})
+    products = Product.products.filter(category=category)
+    return render(request, "store/category.html", {"category": category, "products": products})
