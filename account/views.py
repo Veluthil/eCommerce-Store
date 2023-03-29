b393790d6411ccda5e7e072d821daa9f0d4084a3
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-29 21:32:54 +0200

    Update views.py of account

--- account/views.py
+++ account/views.py
@@ -16,7 +16,7 @@ from .token import account_activation_token
 @login_required()
 def dashboard(request):
     orders = user_orders(request)
-    return render(request, "account/user/dashboard.html", {"orders": orders})
+    return render(request, "account/dashboard/dashboard.html", {"orders": orders})
 
 
 def account_register(request):
@@ -33,7 +33,7 @@ def account_register(request):
             current_site = get_current_site(request)
             subject = "Activate your Account"
             message = render_to_string("account/registration/account_activation_email.html", {
-                "user": user,
+                "dashboard": user,
                 "domain": current_site.domain,
                 "uid": urlsafe_base64_encode(force_bytes(user.pk)),
                 "token": account_activation_token.make_token(user),
@@ -69,7 +69,7 @@ def edit_details(request):
             user_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
-    return render(request, "account/user/edit_details.html", {"user_form": user_form})
+    return render(request, "account/dashboard/edit_details.html", {"user_form": user_form})
 
 
 @login_required()
