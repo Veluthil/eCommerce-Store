71317256c7c5ddf189a1860a19ff7b035462f298
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-29 21:33:31 +0200

    Update path "" name

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    path("", views.product_all, name="product_all"),
+    path("", views.product_all, name="store_home"),
     path("<slug:slug>", views.product_detail, name="product_detail"),
     path("shop/<slug:category_slug>/", views.category_list, name="category_list")
 ]
