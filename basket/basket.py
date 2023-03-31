e61e7b16e28d7a4bbc81a36830718853cbb1fee4
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-31 19:43:41 +0200

    Update to match new database

--- basket/basket.py
+++ basket/basket.py
@@ -18,7 +18,7 @@ class Basket:
     def __iter__(self):
         """Collect the product_id in the session data to query the database and return products."""
         product_ids = self.basket.keys()
-        products = Product.products.filter(id__in=product_ids)
+        products = Product.objects.filter(id__in=product_ids)
         basket = self.basket.copy()
         for product in products:
             basket[str(product.id)]["product"] = product
@@ -51,7 +51,7 @@ class Basket:
         if product_id in self.basket:
             self.basket[product_id]["qty"] = product_qty
         else:
-            self.basket[product_id] = {"price": str(product.price), "qty": int(product_qty)}
+            self.basket[product_id] = {"price": str(product.regular_price), "qty": int(product_qty)}
         self.save_session()
 
     def delete(self, product):
