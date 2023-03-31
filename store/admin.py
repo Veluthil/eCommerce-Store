501f24809fe4ccf2617a63efb7d0e1e77b82cea9
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-03-31 19:45:59 +0200

    Create new classes to use inlines

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,33 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+from .models import *
 
-from .models import Category, Product
+
+admin.site.register(Category, MPTTModelAdmin)
 
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ["name", "slug"]
-    prepopulated_fields = {"slug": ("name",)}
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductSpecificationInline,
+    ]
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductSpecificationValueInline(admin.TabularInline):
+    model = ProductSpecificationValue
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ["title", "author", "slug", "price", "in_stock", "created", "updated"]
-    list_filter = ["in_stock", "is_active"]
-    list_editable = ["price", "in_stock"]
-    prepopulated_fields = {"slug": ("title",)}
+    inlines = [
+        ProductSpecificationValueInline,
+        ProductImageInline,
+    ]
