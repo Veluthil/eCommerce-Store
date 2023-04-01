6c56cd5c3a6ffee0dfaa78b8db0b87e470680d48
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-01 21:52:16 +0200

    Change class UserBase to Customer, create class Address

--- account/models.py
+++ account/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.core.mail import send_mail
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
@@ -6,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomAccountManager(BaseUserManager):
 
-    def create_superuser(self, email, user_name, password, **other_fields):
+    def create_superuser(self, email, name, password, **other_fields):
         other_fields.setdefault("is_staff", True)
         other_fields.setdefault("is_superuser", True)
         other_fields.setdefault("is_active", True)
@@ -14,33 +16,23 @@ class CustomAccountManager(BaseUserManager):
             raise ValueError("Superuser must be assigned to is_staff=True")
         if other_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must be assigned to is_superuser=True")
-        return self.create_user(email, user_name, password, **other_fields)
+        return self.create_user(email, name, password, **other_fields)
 
-    def create_user(self, email, user_name, password, **other_fields):
+    def create_user(self, email, name, password, **other_fields):
         if not email:
             raise ValueError(_("You must provide an email address to continue."))
         email = self.normalize_email(email)
-        user = self.model(email=email, user_name=user_name, **other_fields)
+        user = self.model(email=email, name=name, **other_fields)
         user.set_password(password)
         user.save()
         return user
 
 
-class UserBase(AbstractBaseUser, PermissionsMixin):
+class Customer(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(_("email address"), unique=True)
-    user_name = models.CharField(max_length=100, unique=True)
-    first_name = models.CharField(max_length=100, blank=True)
-    surname = models.CharField(max_length=100, blank=True)
-    about = models.TextField(_("about"), max_length=500, blank=True)
-
-    # Delivery details
-    country = models.CharField(max_length=50, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
-    postcode = models.CharField(max_length=10, blank=True)
-    address_1 = models.CharField(max_length=100, blank=True)
-    address_2 = models.CharField(max_length=100, blank=True)
-    city = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=20, blank=True)
 
     # User status
     is_active = models.BooleanField(default=False)
@@ -51,14 +43,14 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
     objects = CustomAccountManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["user_name"]
+    REQUIRED_FIELDS = ["name"]
 
     class Meta:
         verbose_name = "Accounts"
         verbose_name_plural = "Accounts"
 
     def __str__(self):
-        return self.user_name
+        return self.name
 
     def email_user(self, subject, message):
         send_mail(
@@ -68,3 +60,27 @@ class UserBase(AbstractBaseUser, PermissionsMixin):
             [self.email],
             fail_silently=False,
         )
+
+
+class Address(models.Model):
+    """Address Table."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(Customer, verbose_name=_("Customer"), on_delete=models.CASCADE)
+    full_name = models.CharField(_("Full Name"), max_length=100)
+    phone = models.CharField(_("Phone Number"), max_length=25)
+    postcode = models.CharField(_("Postcode"), max_length=15)
+    address_line_1 = models.CharField(_("Address Line 1"), max_length=150)
+    address_line_2 = models.CharField(_("Address Line 2"), max_length=150)
+    town_city = models.CharField(_("Town/City/State"), max_length=150)
+    delivery_instructions = models.CharField(_("Delivery Instructions"), max_length=500)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+    default = models.BooleanField(_("Default"), default=False)
+
+    class Meta:
+        verbose_name = "Address"
+        verbose_name_plural = "Addresses"
+
+    def __str__(self):
+        return "Address"
