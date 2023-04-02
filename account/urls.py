e43aaf8479cd4282498ce9a9f897d44523e3f777
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-02 21:17:55 +0200

    Uncomment paths for CRUD addresses

--- account/urls.py
+++ account/urls.py
@@ -43,8 +43,8 @@ urlpatterns = [
          name="password_reset_complete"),
     # Addresses
     path("addresses/", views.view_address, name="addresses"),
-    # path("add_address/", views.add_address, name="add_address"),
-    # path("addresses/edit/<slug:id>/", views.edit_address, name="edit_address"),
-    # path("addresses/delete/<slug:id>", views.delete_address, name="delete_address"),
-    # path("addresses/set_default/<slug:id>", views.set_default, name="set_default"),
+    path("add_address/", views.add_address, name="add_address"),
+    path("addresses/edit/<slug:id>/", views.edit_address, name="edit_address"),
+    path("addresses/delete/<slug:id>", views.delete_address, name="delete_address"),
+    path("addresses/set_default/<slug:id>", views.set_default, name="set_default"),
 ]
