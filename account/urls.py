17cdb2e3a8edea2ae430be945f8c6e2cd1824a4c
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-03 21:30:41 +0200

    Add wishlist paths

--- account/urls.py
+++ account/urls.py
@@ -47,4 +47,7 @@ urlpatterns = [
     path("addresses/edit/<slug:id>/", views.edit_address, name="edit_address"),
     path("addresses/delete/<slug:id>", views.delete_address, name="delete_address"),
     path("addresses/set_default/<slug:id>", views.set_default, name="set_default"),
+    # Wish List
+    path("wishlist/", views.wishlist, name="wishlist"),
+    path("wishlist/add_to_wishlist/<int:id>/", views.add_to_wishlist, name="user_wishlist"),
 ]
