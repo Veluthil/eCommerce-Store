d9138fe1ab9b59c348b35f00d0e443e8b929977c
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-05 21:29:10 +0200

    Update Order class

--- orders/models.py
+++ orders/models.py
@@ -6,21 +6,25 @@ from store.models import Product
 
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order_user')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="order_user")
     full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
     address_1 = models.CharField(max_length=250)
     address_2 = models.CharField(max_length=250)
     city = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    post_code = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=20)
+    country_code = models.CharField(max_length=4, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     total_paid = models.DecimalField(max_digits=5, decimal_places=2)
     order_key = models.CharField(max_length=200)
+    payment_option = models.CharField(max_length=200, blank=True)
     billing_status = models.BooleanField(default=False)
 
-    class Meta:
-        ordering = ("-created",)
+
+class Meta:
+    ordering = ("-created",)
 
     def __str__(self):
         return str(self.created)
