a12fad1d0f3392d8abb4a6f7d9cd17a77286d058
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-05 21:31:55 +0200

    Create views for PayPal payment process

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,18 @@
-from django.http import JsonResponse
+import json
+
+from django.contrib import messages
+from django.http import JsonResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from basket.basket import Basket
+from account.models import Address
+from orders.models import Order, OrderItem
 from .models import DeliveryOptions
+
+from paypalcheckoutsdk.orders import OrdersGetRequest
+
+from .paypal import PayPalClient
 
 
 @login_required
@@ -31,3 +40,73 @@ def basket_update_delivery(request):
 
         response = JsonResponse({"total": updated_total_price, "delivery_price": delivery_type.delivery_price})
         return response
+
+
+@login_required
+def delivery_address(request):
+    session = request.session
+    if "purchase" not in request.session:
+        messages.success(request, "Please choose preferred delivery option before proceeding.")
+        return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+    addresses = Address.objects.filter(customer=request.user).order_by("-default")
+
+    if "address" not in request.session:
+        session["address"] = {"address_id": str(addresses[0].id)}
+    else:
+        session["address"]["address_id"] = str(addresses[0].id)
+        session.modified = True
+
+    return render(request, "checkout/delivery_address.html", {"addresses": addresses})
+
+
+@login_required
+def payment_selection(request):
+    session = request.session
+    if "address" not in request.session:
+        messages.success(request, "Please select your address before proceeding.")
+        return HttpResponseRedirect(request.META["HTTP_REFERER"])
+
+    return render(request, "checkout/payment_selection.html")
+
+
+@login_required
+def payment_complete(request):
+    PPClient = PayPalClient()
+
+    body = json.loads(request.body)
+    data = body["orderID"]
+    user_id = request.user.id
+
+    request_order = OrdersGetRequest(data)
+    response = PPClient.client.execute(request_order)
+
+    total_paid = response.result.purchase_units[0].amount.value
+
+    basket = Basket(request)
+    order = Order.objects.create(
+        user_id=user_id,
+        full_name=response.result.purchase_units[0].shipping.name.full_name,
+        email=response.result.payer.email_address,
+        address_1=response.result.purchase_units[0].shipping.address.address_line_1,
+        address_2=response.result.purchase_units[0].shipping.address.admin_area_2,
+        postal_code=response.result.purchase_units[0].shipping.address.postal_code,
+        country_code=response.result.purchase_units[0].shipping.address.country_code,
+        total_paid=response.result.purchase_units[0].amount.value,
+        order_key=response.result.id,
+        payment_option="paypal",
+        billing_status=True,
+    )
+    order_id = order.pk
+
+    for item in basket:
+        OrderItem.objects.create(order_id=order_id, product=item["product"], price=item["price"], quantity=item["qty"])
+
+    return JsonResponse("Payment completed! Thank you.", safe=False)
+
+
+@login_required
+def payment_successful(request):
+    basket = Basket(request)
+    basket.clear()
+    return render(request, "checkout/payment_success.html")
