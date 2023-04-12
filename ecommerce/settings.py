506eb53c2ec259c2c2d15738ec3d20552f495d1a
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-12 20:23:14 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+load_dotenv("D:/Programming/PythonEnV/.env.txt")
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
