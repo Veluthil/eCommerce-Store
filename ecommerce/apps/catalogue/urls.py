f48cfa56f9e4b730d4b7464b45faf4818ddbf9b2
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-20 22:19:02 +0200

    Add set_theme/ path

--- ecommerce/apps/catalogue/urls.py
+++ ecommerce/apps/catalogue/urls.py
@@ -7,5 +7,6 @@ app_name = "catalogue"
 urlpatterns = [
     path("", views.product_all, name="catalogue_home"),
     path("<slug:slug>", views.product_detail, name="product_detail"),
-    path("shop/<slug:slug>/", views.category_list, name="category_list")
+    path("shop/<slug:slug>/", views.category_list, name="category_list"),
+    path("set_theme/", views.set_theme, name="set_theme"),
 ]
