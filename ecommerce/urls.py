88f14caa0fd736154aff46b02974b732c0ebed0f
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-25 20:48:50 +0200

    Update for deployment

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
 ]
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
