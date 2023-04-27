ccab78ffd0edaef0d465f7718a86721d010557d0
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-27 19:02:18 +0200

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,6 +12,9 @@ urlpatterns = [
     path("checkout/", include("ecommerce.apps.checkout.urls", namespace="checkout")),
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
