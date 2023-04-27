51502037636a1359e71d69eaa2d6c9c0de29a6ef
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-27 17:43:36 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#
-# MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = BASE_DIR / 'assets' / 'media/'
+MEDIA_ROOT = BASE_DIR / 'images' / 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles' / 'static'
 STATICFILES_DIRS = [
