dd16dd60b783fb6feebfde14dcaf236a8b0f83ca
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-04-29 00:01:37 +0200

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
