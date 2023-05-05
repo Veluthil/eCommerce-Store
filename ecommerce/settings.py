f6cbcc8387a6cb3e0f0bcf414aaaa6aa87f2501d
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-05-05 20:59:30 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -145,8 +145,7 @@ if AWS:
         'CacheControl': 'max-age=86400',
     }
     AWS_LOCATION = 'static/'
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static',)
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
