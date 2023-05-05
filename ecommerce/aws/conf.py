f4d7b1f11d7490dc2507f16f5492db068ac8db67
Martyna Boradyn <martyna.boradyn@gmail.com>
2023-05-05 20:41:13 +0200

    Update conf.py

--- ecommerce/aws/conf.py
+++ ecommerce/aws/conf.py
@@ -13,15 +13,17 @@ AWS_ACCESS_KEY_ID = os.getenv("ACCESS_KEY")
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = True
-AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = 'public-read'
 
 DEFAULT_FILE_STORAGE = 'django_s3.aws.utils.MediaRootS3BotoStorage'
 STATICFILES_STORAGE = 'django_s3.aws.utils.StaticRootS3BotoStorage'
 AWS_STORAGE_BUCKET_NAME = 'django-ecommerce-dokusho'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 S3DIRECT_REGION = 'eu-central-1'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = S3_URL + 'media/'
-STATIC_URL = S3_URL + 'static/'
+STATIC_LOCATION = 'static'
+PUBLIC_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 AWS_S3_FILE_OVERWRITE = False
 two_months = datetime.timedelta(days=61)
